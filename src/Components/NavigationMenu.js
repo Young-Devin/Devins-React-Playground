162557d05522d9871f8a277a21fac8a6b6d96ed6
Your Name <you@example.com>
2020-07-22 14:51:12 -0400

    Updated navigation menu

--- src/Components/NavigationMenu.js
+++ src/Components/NavigationMenu.js
@@ -14,16 +14,7 @@ function NavigationMenu(props){
                         className="text-blue-500 py-3 border-t border-b block"
                         onClick={props.closeMenu}
                     >
-                        Gallery
-                    </Link>
-                </li>
-                <li>
-                    <Link 
-                        to="/about" 
-                        className="text-blue-500 py-3 border-b block"
-                        onClick={props.closeMenu}
-                    >
-                        About
+                        Products
                     </Link>
                 </li>
                 <li>
@@ -33,6 +24,15 @@ function NavigationMenu(props){
                         onClick={props.closeMenu}
                     >
                         Students
+                    </Link>
+                </li>
+                <li>
+                    <Link 
+                        to="/about" 
+                        className="text-blue-500 py-3 border-b block"
+                        onClick={props.closeMenu}
+                    >
+                        About
                     </Link>
                 </li>
 {/*                 <li>
